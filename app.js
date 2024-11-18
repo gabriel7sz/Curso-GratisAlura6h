4d2acfcc290fd69e6de4888b1b23cdecef7d4474
gabriel7sz <gabriel7sz@outlook.com>
2024-11-18 12:00:00 -0300

    updates

--- app.js
+++ app.js
@@ -1,69 +1,23 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 8;
-console.log(numeroSecreto)
-let chute = prompt('Escolha um número entre 1 e 10');
+let numeroMaximo = 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
 
-if (chute == numeroSecreto){
-    alert(`Isso aí, Você descobriu o número secreto (${numeroSecreto})!`);
-} else {
-    alert('Você errou :(');
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+    if (chute == numeroSecreto){
+        break;
+    } else {
+        if (chute > numeroSecreto){
+            alert(`O número secreto é menor que ${chute}!`);
+        } else {
+            alert(`O número secreto é maior que ${chute}!`);
+        }
+        tentativas++ ;
+    }       
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// desafio 1
-// let diaSemana = prompt('Qual é o dia da semana? ')
-// console.log(`dia da semana: ${diaSemana}`)
-// if (diaSemana == 'sabado' || diaSemana == 'domingo') {
-//     alert('Bom final de semana!')
-// } else {
-//     alert('Boa semana!')
-// }
-
-// desafio 2
-// let numero = prompt('Informe qualquer número natural: ')
-// console.log(`O número natural é: ${numero}`)
-// if (numero >= 0) {
-//     alert(`${numero} é positivo!`)
-// } else {
-//     alert(`${numero} é negativo!`)
-// }
-
-// desafio 3
-// let pontuacao = prompt('Digite a pontuação: ')
-// if (pontuacao >= 100) {
-//     alert(`A pontuação foi ${pontuacao}! Parabéns, você venceu!`)
-// } else {
-//     alert(`A pontuação foi ${pontuacao}! Tente novamente para ganhar.`)
-// }
-
-// desafio 4 
-// let saldo = 500
-// let deposito
-// alert(`Seu saldo atual é: R$${saldo},00`)
-
-// let escolha = prompt('Digite 1 para despositar ou 2 para sair')
-// if(escolha == 1) {
-//     let deposito = prompt(`Quanto deseja depositar? `)
-//     resultado = parseInt(saldo) + parseInt(deposito)
-//     alert(`Seu saldo agora é R$${resultado},00`)  
-// } else if (escolha == 2) {
-//     alert(`Obrigado pelo uso de nosso serviço. Seu saldo continua em R$${saldo},00`)
-// } 
-
-// desafio 5
-// let nome = prompt(`Digite seu nome, por favor: `)
-// alert(`Bem Vindo(a) ${nome}!`)
+// formatando texto plural e singular
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso aí, Você descobriu o número secreto ${numeroSecreto} em ${tentativas} ${palavraTentativa}.`);
---
+++ desafios.js
@@ -0,0 +1,114 @@
+// desafio 1
+// let diaSemana = prompt('Qual é o dia da semana? ')
+// console.log(`dia da semana: ${diaSemana}`)
+// if (diaSemana == 'sabado' || diaSemana == 'domingo') {
+//     alert('Bom final de semana!')
+// } else {
+//     alert('Boa semana!')
+// }
+
+// desafio 2
+// let numero = prompt('Informe qualquer número natural: ')
+// console.log(`O número natural é: ${numero}`)
+// if (numero >= 0) {
+//     alert(`${numero} é positivo!`)
+// } else {
+//     alert(`${numero} é negativo!`)
+// }
+
+// desafio 3
+// let pontuacao = prompt('Digite a pontuação: ')
+// if (pontuacao >= 100) {
+//     alert(`A pontuação foi ${pontuacao}! Parabéns, você venceu!`)
+// } else {
+//     alert(`A pontuação foi ${pontuacao}! Tente novamente para ganhar.`)
+// }
+
+// desafio 4 
+// let saldo = 500
+// let deposito
+// alert(`Seu saldo atual é: R$${saldo},00`)
+
+// let escolha = prompt('Digite 1 para despositar ou 2 para sair')
+// if(escolha == 1) {
+//     let deposito = prompt(`Quanto deseja depositar? `)
+//     resultado = parseInt(saldo) + parseInt(deposito)
+//     alert(`Seu saldo agora é R$${resultado},00`)  
+// } else if (escolha == 2) {
+//     alert(`Obrigado pelo uso de nosso serviço. Seu saldo continua em R$${saldo},00`)
+// } 
+
+// desafio 5
+// let nome = prompt(`Digite seu nome, por favor: `)
+// alert(`Bem Vindo(a) ${nome}!`)
+
+// desafio 6
+// let i = 0;
+// while (i < 10) {
+//     i++;
+//     console.log(i);
+// }
+
+// desafio 7 
+// let i = 10;
+// while (i > 0) {
+//     i--
+//     console.log(i);
+// }
+
+// desafio 8
+// let numero = prompt('Digite um número: ');
+// while (numero > 0) {
+//     numero -- ;
+//     console.log(numero);
+// }
+
+// desafio 9
+// let numeroMaximo = prompt("Digite um número para a contagem progregressiva:");;
+// let contador = 0;
+
+// while (contador <= numeroMaximo) {
+//     console.log(contador);
+//     contador++
+// }
+
+// desafio 10
+// let valor1 = prompt(`Digite um valor: `);
+// let valor2 = prompt(`Digite um valor: `);
+// resultado = parseInt(valor1) + parseInt(valor2);
+// console.log(resultado);
+
+//desafio 11
+// let idade = prompt(`Insira sua idade: `);
+// if (idade >= 18) {
+//     console.log(`Você é maior de idade!`);
+// } else {
+//     console.log(`Você é menor de idade!`);   
+// }
+
+// desafio 12
+// let numero = prompt(`Digite um número: `);
+// if (numero >= 0) {
+//     console.log(`O número ${numero} é positivo!`);
+// } else {
+//     console.log(`O número ${numero} é negativo!`);
+// }
+
+// desafio 13
+// let i = 0;
+// while (i < 10) {
+//     console.log(`O número ${i} ainda é menor que 10`);
+//     i++
+// }
+
+// desafio 14
+// let nota = prompt(`Digite qual foi sua nota: `);
+// if (nota < 7) {
+//     console.log('Você foi reprovado!');
+// } else {
+//     console.log('Você foi aprovado!');
+// }
+
+// desafio 15
+// let numeroAleatorio = parseInt(Math.random() * 10 + 1);
+// console.log(numeroAleatorio);
